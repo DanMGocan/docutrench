8aa80d82915581099663d130ca625f6e0d4034aa
Dan Gocan <dangocan@outlook.com>
2023-12-27 12:17:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# DocuTrench! 
+
+## What is?
+**DocuTrench** is a very simple document management system that uses an e-mail pipeline to help you keep track of your documents, invoices, etc. 
+Using Google APIs, it extracts document straight from an e-mail inbox and adds them to a dashboard. All your vendors have to do is e-mail *your_company*_*their_company*@docutrench.com with any attachments and the documents will be available in your dashboard in a matter of seconds. 
+
+## Status
+This project is heavily work-in-progress and it is not usable as is. 
+
+## Technology 
+* Flask,
+* Bootstrap,
+* PostgreSQL  
+
---
+++ email_module.py
@@ -0,0 +1,99 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import os
+import io
+import base64
+
+# Path to your service account key file
+SERVICE_ACCOUNT_FILE = 'docutrenchservicekey.json'
+
+# Scopes for full Gmail access
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.modify'
+    ]
+
+def gmail_authenticate():
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    # If accessing a user's Gmail in a Google Workspace domain, uncomment the next line
+    credentials = credentials.with_subject('dan@docutrench.com')
+
+    service = build('gmail', 'v1', credentials=credentials)
+    return service
+
+def get_email_details(service, user_id, message_id, store_dir, txt_dir):
+    message = service.users().messages().get(userId=user_id, id=message_id, format='full').execute()
+
+    # Extract headers for subject and recipient(s)
+    headers = message['payload']['headers']
+    subject = next(header['value'] for header in headers if header['name'] == 'Subject')
+    to = next(header['value'] for header in headers if header['name'] == 'To')
+
+    # Extract body
+    body = ""
+    if 'parts' in message['payload']:
+        # Handle multipart email
+        for part in message['payload']['parts']:
+            if part['mimeType'] == 'text/plain':
+                body += base64.urlsafe_b64decode(part['body']['data'].encode('UTF-8')).decode('utf-8')
+            elif 'attachmentId' in part['body']:
+                # Handle attachments
+                file_data = get_attachment(service, user_id, message_id, part['body']['attachmentId'])
+                filename = part.get('filename', 'attachment')
+                path = os.path.join(store_dir, filename)
+                with open(path, 'wb') as f:
+                    f.write(file_data)
+    else:
+        # Handle non-multipart email
+        body = base64.urlsafe_b64decode(message['payload']['body']['data'].encode('UTF-8')).decode('utf-8')
+
+    # Save email details to a text file
+    email_txt_filename = f"email_{message_id}.txt"
+    with open(os.path.join(txt_dir, email_txt_filename), 'w', encoding='utf-8') as txt_file:
+        txt_file.write(f"Subject: {subject}\n")
+        txt_file.write(f"To: {to}\n")
+        txt_file.write("Body:\n")
+        txt_file.write(body)
+
+    return {'subject': subject, 'to': to, 'body': body, 'txt_filename': email_txt_filename}
+
+def get_attachment(service, user_id, message_id, attachment_id):
+    attachment = service.users().messages().attachments().get(userId=user_id, messageId=message_id, id=attachment_id).execute()
+    file_data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
+    return file_data
+
+def delete_message(service, user_id, message_id):
+    pass
+    # try:
+    #     service.users().messages().delete(userId=user_id, id=message_id).execute()
+    #     print(f"Deleted message {message_id}")
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")
+
+def list_messages_and_process(service, user_id='me', store_dir='attachments', txt_dir='emails_txt'):
+    # Ensure the directories exist
+    os.makedirs(store_dir, exist_ok=True)
+    os.makedirs(txt_dir, exist_ok=True)
+
+    # Call the Gmail API to fetch messages
+    results = service.users().messages().list(userId=user_id).execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        print("No messages found.")
+    else:
+        for message in messages:
+            msg_id = message['id']
+            email_details = get_email_details(service, user_id, msg_id, store_dir, txt_dir)
+            print(f"Email details saved to {email_details['txt_filename']}")
+
+            delete_message(service, user_id, msg_id)
+
+    return messages
+
+
+
+# Authenticate and process messages
+service = gmail_authenticate()
+list_messages_and_process(service)
